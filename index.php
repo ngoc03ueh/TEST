a2819e91bece530fde83fe33a7f1d2885695a831
ngoc03ueh <ngocpham.031003@gmail.com>
2024-03-07 12:29:21 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,427 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Khu liên hợp thể thao Nguyễn Tri Phương</title>
+  <link rel="stylesheet" type="text/css" href="styles/home.css" />
+  <script type="text/javascript" src="./scripts/home.js" language="javascript"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+  <link rel="apple-touch-icon" sizes="57x57" href="./favicon/apple-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="60x60" href="./favicon/apple-icon-60x60.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="./favicon/apple-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="./favicon/apple-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="./favicon/apple-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="./favicon/apple-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="./favicon/apple-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="./favicon/apple-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-icon-180x180.png" />
+  <link rel="icon" type="image/png" sizes="192x192" href="./favicon/android-icon-192x192.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="96x96" href="./favicon/favicon-96x96.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png" />
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="msapplication-TileColor" content="#ffffff" />
+  <meta name="msapplication-TileImage" content="./favicon/ms-icon-144x144.png" />
+  <meta name="theme-color" content="#ffffff" />
+</head>
+
+<body>
+  <div class="trang-ch-khch">
+    <header class="header">
+      <div class="header1">
+        <div class="text">
+          <div class="rectangle">
+            <img class="frame-icon" loading="lazy" alt="" src="image/home-img/rectangle-3@2x.png" />
+
+            <b class="khu-lin-hp-container">
+              <span>Khu liên hợp thể thao </span>
+              <span class="nguyn-tri-phng">Nguyễn Tri Phương</span>
+            </b>
+          </div>
+          <div class="btn">
+            <div class="btn-login">
+              <b class="ng-nhp">Đăng ký</b>
+            </div>
+            <div class="btn-login1">
+              <b class="ng-nhp1">Đăng nhập</b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="instance-wrapper">
+        <div class="instance">
+          <div class="tri-nghim-t-sn-th-thao-t-wrapper">
+            <div class="tri-nghim-t">
+              Trải nghiệm đặt sân thể thao trực tuyến cùng NTP Sports Hub
+            </div>
+          </div>
+          <div class="text1">
+            <div class="instance-container">
+              <div class="instance1">
+                <div class="vector">
+                  <img class="frame-icon1" alt="" src="./image/home-img/frame.svg" />
+                </div>
+                <input class="input" placeholder="Tìm kiếm" type="text" />
+              </div>
+            </div>
+            <button class="divflex">
+              <div class="vector-wrapper">
+                <img class="vector-icon" alt="" src="./image/home-img/vector-1.svg" />
+              </div>
+              <div class="tt-c-loi">Tất cả loại sân</div>
+              <img class="logo-icon" alt="" src="./image/home-img/vector-2.svg" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="navigation">
+        <div class="menu-button">
+          <div class="menu">
+            <div class="bng-7">Bóng đá (7)</div>
+            <div class="bng-chuyn-4">Bóng chuyền (4)</div>
+            <div class="bng-r-3">Bóng rổ (3)</div>
+            <div class="cu-lng-7">Cầu lông (7)</div>
+            <div class="tennis-5">Tennis (5)</div>
+          </div>
+          <div class="magnifying-glass-parent">
+            <div class="magnifying-glass"></div>
+            <div class="frame-child"></div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+    <section class="categories">
+      <div class="event">
+        <div class="banner">
+          <img class="banner-img" alt="" src="./image/home-img/image-51@2x.png" />
+
+          <div class="move-right">
+            <img class="img-icon" loading="lazy" alt="" src="./image/home-img/img.svg" />
+          </div>
+          <div class="move-left">
+            <img class="img-icon1" loading="lazy" alt="" src="./image/home-img/img.svg" />
+          </div>
+        </div>
+        <div class="event-list">
+          <div class="item"></div>
+          <div class="item1"></div>
+          <div class="item2"></div>
+          <div class="item3"></div>
+          <div class="item4"></div>
+          <div class="item5"></div>
+          <div class="item6"></div>
+        </div>
+      </div>
+      <div class="outstanding-court-container">
+          <h2 class="top-3">Top 3 sân thể thao nổi bật</h2>
+        <div class="range-input">
+
+
+          <div class="outstanding-court-card">
+            <div class="image-stats-container">
+              <img class="outstanding-court-img" alt="" src="./image/home-img/image-3-2@2x.png" />
+
+              <div class="order-quantity">
+                <img class="vector-icon3" alt="" src="./image/home-img/vector-3.svg" />
+
+                <div class="quantity">12 lượt đặt sân</div>
+              </div>
+              <div class="frame-div">
+                <img class="vector-icon4" loading="lazy" alt="" src="./image/home-img/vector-4.svg" />
+                <div class="top2">TOP</div>
+              </div>
+            </div>
+            <div class="details-container">
+              <div class="outstanding-court-info">
+                <div class="court-info-name-price">
+                  <div class="court-name">Sân cầu lông số 1</div>
+                  <div class="court-price">
+                    <span class="span4">đ</span>150.000/h -
+                    <span class="span5">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="outstanding-rating">
+                  <div class="rating-value">5.0</div>
+                  <img class="rating-icon" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="outstanding-court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+          
+         
+          <div class="outstanding-court-card">
+            <div class="image-stats-container">
+              <img class="outstanding-court-img" alt="" src="./image/home-img/image-3-2@2x.png" />
+
+              <div class="order-quantity">
+                <img class="vector-icon3" alt="" src="./image/home-img/vector-3.svg" />
+
+                <div class="quantity">12 lượt đặt sân</div>
+              </div>
+              <div class="frame-div">
+                <img class="vector-icon4" loading="lazy" alt="" src="./image/home-img/vector-4.svg" />
+                <div class="top2">TOP</div>
+              </div>
+            </div>
+            <div class="details-container">
+              <div class="outstanding-court-info">
+                <div class="court-info-name-price">
+                  <div class="court-name">Sân cầu lông số 1</div>
+                  <div class="court-price">
+                    <span class="span4">đ</span>150.000/h -
+                    <span class="span5">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="outstanding-rating">
+                  <div class="rating-value">5.0</div>
+                  <img class="rating-icon" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="outstanding-court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+   
+          <div class="outstanding-court-card">
+            <div class="image-stats-container">
+              <img class="outstanding-court-img" alt="" src="./image/home-img/image-3-2@2x.png" />
+
+              <div class="order-quantity">
+                <img class="vector-icon3" alt="" src="./image/home-img/vector-3.svg" />
+
+                <div class="quantity">12 lượt đặt sân</div>
+              </div>
+              <div class="frame-div">
+                <img class="vector-icon4" loading="lazy" alt="" src="./image/home-img/vector-4.svg" />
+                <div class="top2">TOP</div>
+              </div>
+            </div>
+            <div class="details-container">
+              <div class="outstanding-court-info">
+                <div class="court-info-name-price">
+                  <div class="court-name">Sân cầu lông số 1</div>
+                  <div class="court-price">
+                    <span class="span4">đ</span>150.000/h -
+                    <span class="span5">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="outstanding-rating">
+                  <div class="rating-value">5.0</div>
+                  <img class="rating-icon" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="outstanding-court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+
+       
+
+        </div>
+
+
+
+
+      </div>
+      <div class="court-list">
+        <div class="header-title">
+          <h2 class="title">Đặt lịch sân thể thao</h2>
+        </div>
+        <div class="contact-info">
+
+
+          <div class="court-card">
+            <div class="court-image-wrapper">
+              <img class="court-image" loading="lazy" alt="" src="./image/home-img/image-3-3@2x.png" />
+            </div>
+            <div class="court-detail-container">
+              <div class="court-info">
+                <div class="court-name-price">
+                  <div class="c-name">Sân bóng đá số 2</div>
+                  <div class="c-price">
+                    <span class="span6">đ</span>150.000/h -
+                    <span class="span7">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="rating-court">
+                  <div class="value">5.0</div>
+                  <img class="rating-court-img" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+
+          <div class="court-card">
+            <div class="court-image-wrapper">
+              <img class="court-image" loading="lazy" alt="" src="./image/home-img/image-3-3@2x.png" />
+            </div>
+            <div class="court-detail-container">
+              <div class="court-info">
+                <div class="court-name-price">
+                  <div class="c-name">Sân bóng đá số 2</div>
+                  <div class="c-price">
+                    <span class="span6">đ</span>150.000/h -
+                    <span class="span7">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="rating-court">
+                  <div class="value">5.0</div>
+                  <img class="rating-court-img" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+
+          <div class="court-card">
+            <div class="court-image-wrapper">
+              <img class="court-image" loading="lazy" alt="" src="./image/home-img/image-3-3@2x.png" />
+            </div>
+            <div class="court-detail-container">
+              <div class="court-info">
+                <div class="court-name-price">
+                  <div class="c-name">Sân bóng đá số 2</div>
+                  <div class="c-price">
+                    <span class="span6">đ</span>150.000/h -
+                    <span class="span7">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="rating-court">
+                  <div class="value">5.0</div>
+                  <img class="rating-court-img" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+
+          <div class="court-card">
+            <div class="court-image-wrapper">
+              <img class="court-image" loading="lazy" alt="" src="./image/home-img/image-3-3@2x.png" />
+            </div>
+            <div class="court-detail-container">
+              <div class="court-info">
+                <div class="court-name-price">
+                  <div class="c-name">Sân bóng đá số 2</div>
+                  <div class="c-price">
+                    <span class="span6">đ</span>150.000/h -
+                    <span class="span7">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="rating-court">
+                  <div class="value">5.0</div>
+                  <img class="rating-court-img" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+
+          <div class="court-card">
+            <div class="court-image-wrapper">
+              <img class="court-image" loading="lazy" alt="" src="./image/home-img/image-3-3@2x.png" />
+            </div>
+            <div class="court-detail-container">
+              <div class="court-info">
+                <div class="court-name-price">
+                  <div class="c-name">Sân bóng đá số 2</div>
+                  <div class="c-price">
+                    <span class="span6">đ</span>150.000/h -
+                    <span class="span7">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="rating-court">
+                  <div class="value">5.0</div>
+                  <img class="rating-court-img" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+          <div class="court-card">
+            <div class="court-image-wrapper">
+              <img class="court-image" loading="lazy" alt="" src="./image/home-img/image-3-3@2x.png" />
+            </div>
+            <div class="court-detail-container">
+              <div class="court-info">
+                <div class="court-name-price">
+                  <div class="c-name">Sân bóng đá số 2</div>
+                  <div class="c-price">
+                    <span class="span6">đ</span>150.000/h -
+                    <span class="span7">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="rating-court">
+                  <div class="value">5.0</div>
+                  <img class="rating-court-img" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+
+          <div class="court-card">
+            <div class="court-image-wrapper">
+              <img class="court-image" loading="lazy" alt="" src="./image/home-img/image-3-3@2x.png" />
+            </div>
+            <div class="court-detail-container">
+              <div class="court-info">
+                <div class="court-name-price">
+                  <div class="c-name">Sân bóng đá số 2</div>
+                  <div class="c-price">
+                    <span class="span6">đ</span>150.000/h -
+                    <span class="span7">đ</span>240.000/h
+                  </div>
+                </div>
+                <button class="rating-court">
+                  <div class="value">5.0</div>
+                  <img class="rating-court-img" alt="" src="./image/home-img/vuesaxboldstar.svg" />
+                </button>
+              </div>
+              <img class="court-type" loading="lazy" alt="" src="./image/home-img/bng-a-img@2x.png" />
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      <div class="load-more-wrapped">
+        <button class="load-more-button">
+          <b class="load-more">Xem thêm</b>
+        </button>
+      </div>
+    </section>
+
+
+  </div>
+  <?php include "./footer/footer.php"; ?>
+
+
+  <!-- code phần body -->
+  <script>
+    function changeFooterImageSrc() {
+      var NTPLogo = document.getElementById("NTP-logo-img");
+      NTPLogo.src = "./image/footer-img/footer-logo.svg";
+
+      var phone = document.getElementById("phone-img");
+      phone.src = "./image/footer-img/call.svg";
+
+      var address = document.getElementById("address-img");
+      address.src = "./image/footer-img/location.svg";
+
+      var facebook = document.getElementById("facebook-img");
+      facebook.src = "./image/footer-img/facebook.svg";
+
+      var email = document.getElementById("email-img");
+      email.src = "./image/footer-img/mail-opened.svg";
+    }
+    changeFooterImageSrc()
+  </script>
+</body>
+
+</html>
